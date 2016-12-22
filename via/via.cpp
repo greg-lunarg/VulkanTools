accc7f627fb511f47e81dfd2f2d1a15d7e1be713
Mark Young <marky@lunarg.com>
2016-12-22 13:07:45 -0700

    via: Fix driver detection
    If driver is installed in a non-standard place, VIA used to
    not properly detect it.
    
    Change-Id: I520e9d4d9faab9a49359c0f1ee299edaf6f65a47

--- via/via.cpp
+++ via/via.cpp
@@ -2840,18 +2840,8 @@ bool ReadDriverJson(std::string cur_driver_json, bool &found_lib) {
                     found_lib = true;
                 }
             }
-            if (!found_lib) {
-                snprintf(generic_string, MAX_STRING_LENGTH - 1,
-                         "Failed to find driver %s "
-                         "referenced by JSON %s",
-                         full_driver_path, cur_driver_json.c_str());
-                PrintBeginTableRow();
-                PrintTableElement("");
-                PrintTableElement("");
-                PrintTableElement(generic_string);
-                PrintEndTableRow();
-            }
-        } else {
+        }
+        if (!found_lib) {
             FILE *fp;
             sprintf(generic_string,
                     "/sbin/ldconfig -v -N -p | grep %s | awk \'{ print $4 }\'",
@@ -2864,7 +2854,7 @@ bool ReadDriverJson(std::string cur_driver_json, bool &found_lib) {
                          driver_name.c_str(), cur_driver_json.c_str());
                 PrintBeginTableRow();
                 PrintTableElement("");
-                PrintTableElement("ERROR");
+                PrintTableElement("");
                 PrintTableElement(generic_string);
                 PrintEndTableRow();
             } else {
